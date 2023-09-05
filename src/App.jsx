f26e8c49e5b84583145e17ef403ae229b1fb13ad
James Njenga <knjenga94@gmail.com>
2023-09-05 18:02:50 +0300

    Create and style cards. Add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero/>
+      <HeadlineCards/>
     </div>
   );
 }
