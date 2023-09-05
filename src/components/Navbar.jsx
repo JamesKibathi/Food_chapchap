fca726483a834143c7f7375a6da554ac11a13a94
James Njenga <knjenga94@gmail.com>
2023-09-05 16:33:50 +0300

    Add state to toggle netween mobile menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 import { AiFillTag, AiOutlineClose, AiOutlineMenu, AiOutlineSearch } from "react-icons/ai";
 import {BsFillCartFill, BsFillSaveFill} from "react-icons/bs"
@@ -7,12 +7,14 @@ import {FaUserFriends, FaWallet} from "react-icons/fa"
 import {MdFavorite,MdHelp} from "react-icons/md"
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between 
     items-center p-4">
         {/**left side */}
       <div className="flex items-center">
-        <div className="cursor-pointer">
+        <div onClick={()=>setNav(!nav)} className="cursor-pointer">
           <AiOutlineMenu size={30} />
         </div>
         <h1 className="text-2xl sm:text-3xl lg:text-4xl px-2">
@@ -38,12 +40,14 @@ const Navbar = () => {
       </button>
       {/* Mobile menu */}
       {/* Overlay */}
-      <div className="bg-black/80 fixed w-full h-screen z-10
-      top-0 left-0"></div>
+      {nav ? <div className="bg-black/80 fixed w-full h-screen z-10
+      top-0 left-0"></div> :""}
+      
       {/*Side drawer menu*/}
-      <div className="fixed top-0 left-0 w-[300px] h-screen bg-white
-      z-10 duration-300">
-        <AiOutlineClose size={30} className="absolute right-4 top-4 cursor-pointer"/>
+      <div className={nav ? "fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300":"fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300"}>
+        <AiOutlineClose
+        onClick={()=>setNav(!nav)}
+         size={30} className="absolute right-4 top-4 cursor-pointer"/>
         <h2 className="text-2xl p-4">Food <span className="font-bold">Chapchap</span></h2>
         <nav>
             <ul className="flex flex-col p-4 text-gray-800">
