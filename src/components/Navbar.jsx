e883271be8b3ef26216953f139f4738e1792ba12
James Njenga <knjenga94@gmail.com>
2023-09-05 15:02:13 +0300

    Install react components, add delivery and pick up menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
-const Navbar = () =>{
-    return(
-        <div>Navbar</div>
-    )
-}
+import { AiOutlineMenu } from "react-icons/ai";
+const Navbar = () => {
+  return (
+    <div>
+        {/**left side */}
+      <div>
+        <div>
+          <AiOutlineMenu size={30} />
+        </div>
+        <h1>
+          Food <span>Chapchap</span>
+        </h1>
+        <div>
+            <p>Delivery</p>
+            <p>Pickup</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
